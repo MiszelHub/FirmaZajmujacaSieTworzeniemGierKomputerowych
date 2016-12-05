54d6a38f824a794030aea188198aafcfe3128fb2
Miszel66 <nolanb.o.s@gmail.com>
2016-12-05 13:56:53 +0100

    dodałem projetk do bazy danych

---
+++ Solution1/WamesRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WamesRepository
+{
+    interface IRepository<T>
+    {
+        void Add(T Entity);
+
+    }
+}
